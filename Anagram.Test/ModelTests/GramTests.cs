da84bb6740875488151cd14358eda638c32d3f1c
dustin herboldshimer and heather zurek <heather@>
2019-04-24 14:13:03 -0700

    add 5 passing tests

---
+++ Anagram.Test/ModelTests/GramTests.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnagramMaker.Models;
+
+namespace Anagram.TestTools
+{
+  [TestClass]
+  public class GramTest
+  {
+    // public void Dispose()
+    // {
+    //   Gram.ClearAll();
+    // }
+
+    [TestMethod]
+    public void GramConstructor_CreateInstanceOfGram_Gram()
+    {
+      Gram newGram = new Gram("cracker");
+      Assert.AreEqual(typeof(Gram), newGram.GetType());
+    }
+
+    [TestMethod]
+    public void GetWord_ReturnsWord_String()
+    {
+      //arrange
+      string word = "cracker";
+      Gram newGram = new Gram(word);
+      //act
+      string result = newGram.GetWord();
+      //assert
+      Assert.AreEqual(word, result);
+    }
+
+    [TestMethod]
+    public void SetWord_SetWord_String()
+    {
+      //Arrange
+    string word = "cracker";
+    Gram newGram = new Gram(word);
+
+    //Act
+    string updatedWord = "biscuit";
+    newGram.SetWord(updatedWord);
+    string result = newGram.GetWord();
+
+    //Assert
+    Assert.AreEqual(updatedWord, result);
+    }
+
+    [TestMethod]
+    public void GetWordList_ReturnsGrams_GramList()
+    {
+      Gram newGram = new Gram("wordOne");
+      List<string> newList = new List<string> { };
+      //Act
+      List<string> result = newGram.GetWordList();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void AddWord_AddWordtoWordList_ItemList()
+    {
+      //Arrange
+      string word = "dough";
+      Gram newGram = new Gram("hug");
+      List<string> newList = new List<string> { word };
+
+      //Act
+      newGram.AddWord(word);
+      List<string> result = newGram.GetWordList();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+  }
+}
---
+++ Anagram/Models/Gram.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+
+namespace AnagramMaker.Models
+{
+  public class Gram
+  {
+    private string _word;
+    private List<string> _wordList = new List<string> {};
+
+    // public static void ClearAll()
+    // {
+    //   _instances.Clear();
+    // }
+
+    public Gram(string word)
+    {
+      _word = word;
+    }
+
+    public string GetWord()
+    {
+      return _word;
+    }
+
+    public void SetWord(string userWord)
+    {
+      _word = userWord;
+    }
+
+    public List<string> GetWordList()
+    {
+      return _wordList;
+    }
+
+    public void AddWord(string word)
+    {
+      _wordList.Add(word);
+    }
+
+  }
+}
